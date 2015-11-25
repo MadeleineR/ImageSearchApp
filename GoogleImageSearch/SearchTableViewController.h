0846f9abfd4c9af443d932da3ea9240dc8afe58a
Client7 <client7@mobileinno-client7s-mac-mini.local>
2015-11-25 15:32:58 +0100

    TableView with Searchbar added

---
+++ GoogleImageSearch/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  GoogleImageSearch
+//
+//  Created by Client7 on 25.11.15.
+//  Copyright © 2015 fhtw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewController : UITableViewController
+
+@end
